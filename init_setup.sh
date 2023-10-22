cd5d9e8401d46c50093555bfe021047a978e1e27
Sagar Khatale <sagarkhatale@gmail.com>
2023-10-22 11:50:54 +0530

    corrected conda activate syntax

--- init_setup.sh
+++ init_setup.sh
@@ -6,7 +6,7 @@ conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the environment" 
 
-source activate ./env
+conda activate ./env
 
 echo [$(date)]: "installing the dev requirements" 
 
